8ea5bbc68372939190bd7d65b7db80f3cd0d4456
Andrew Chang <achang97@stanford.edu>
2017-11-17 01:02:13 -0800

    corrected logisticModel

--- pathUCS.py
+++ pathUCS.py
@@ -1,7 +1,9 @@
 import searchUtil
-from evaluateCrime import LocationGrid
+from evaluateCrime import LocationGrid, SafePathFeatureExtractor
 from sklearn.externals import joblib
 import math
+import numpy
+import matplotlib.pyplot as plt
 
 
 def getStringAction(dr, dc):
@@ -19,6 +21,33 @@ def getStringAction(dr, dc):
 
 	return resultString
 
+def getDelta(directionString):
+	change = [0, 0]
+	
+	if 'L' in directionString:
+		change[1] = -1
+	elif 'R' in directionString:
+		change[1] = 1
+
+	if 'U' in directionString:
+		change[0] = -1
+	elif 'D' in directionString:
+		change[0] = 1
+
+	return change
+
+
+def displayResults(actions, start):
+
+	print start
+	
+	for action in actions:
+		print '\n{}'.format(action)
+
+		dr, dc = getDelta(action)
+		start = (start[0] + dr, start[1] + dc)
+		print start
+
 
 class ShortestPath(searchUtil.SearchProblem):
 	# very simple distance UCS, just
@@ -34,40 +63,57 @@ class ShortestPath(searchUtil.SearchProblem):
         return self.start
 
     def isEnd(self, state):
-        return state == end
+        return state == self.end
 
     def succAndCost(self, state):
         results = []
 
         for dr in range(-1, 2):
         	for dc in range(-1, 2):
-
-        		if dr == 0 and dc == 0:
+        		if (dr == 0 and dc == 0):
         			continue
 
-        		newLocation = (state[0] + dr, state[1] + dc)
-        		results.append((getStringAction(dr, dc), newLocation, math.sqrt(dr ** 2 + dc ** 2)))
+        		newR = state[0] + dr
+        		newC = state[1] + dc
+
+        		if not self.locationGrid.inBounds(newR, newC):
+        			continue
+
+        		results.append((getStringAction(dr, dc), (newR, newC), math.sqrt(dr ** 2 + dc ** 2)))
 
         return results
 
 
-# class SafestPath(searchUtil.SearchProblem):
-#     def __init__(self, start, end, mileBlockSize, modelFile):
+class SafestPath(searchUtil.SearchProblem):
+    def __init__(self, start, end, locationGrid):
+        self.start = start
+        self.end = end
+        self.locationGrid = locationGrid 
 
-#         self.start = start
+    def startState(self):
+        return self.start
 
-#     def startState(self):
-#         return 0
+    def isEnd(self, state):
+        return state == self.end
 
-#     def isEnd(self, state):
-#         return state == len(self.query)
+    def succAndCost(self, state):
+        results = []
 
-#     def succAndCost(self, state):
-#         results = []
-#         for i in range(state, len(self.query)):
-#             newUnigram = self.query[state:i+1]
-#             results.append((newUnigram, i + 1, self.unigramCost(newUnigram)))
-#         return results
+        for dr in range(-1, 2):
+        	for dc in range(-1, 2):
+        		if (dr == 0 and dc == 0):
+        			continue
+
+        		newR = state[0] + dr
+        		newC = state[1] + dc
+
+        		if not self.locationGrid.inBounds(newR, newC):
+        			continue
+
+        		results.append((getStringAction(dr, dc), (newR, newC), \
+        			self.locationGrid.locationGrid[newR][newC]))
+
+        return results 
 
 
 if __name__ == '__main__':
@@ -78,14 +124,40 @@ if __name__ == '__main__':
 	bottomRight = (41.640738, -87.510901)
 
 	locationGrid = LocationGrid(mileBlockSize, topLeft, bottomRight)
+	logreg = joblib.load('logisticModel.pkl')
+
+	featureExtractor = SafePathFeatureExtractor(['Row', 'Col', 'Day', 'Hr'], \
+		[locationGrid.numRows(), locationGrid.numCols(), 7, 24])
+
+	for i in range(locationGrid.numRows()):
+		for j in range(locationGrid.numCols()):
+			# walking on Tuesday at 11 PM
+			sample = featureExtractor.extract([i, j, 2, 23])
+			predicted_prob = logreg.predict_proba(sample)
+			locationGrid.locationGrid[i][j] = predicted_prob[0][1]
+
+	# # Generate some test data
+	# data = numpy.array(locationGrid.locationGrid)
+
+	# plt.title('Crime Probabilities')
+	# plt.imshow(data, cmap='hot', interpolation='nearest')
+	# plt.colorbar()
+	# plt.show()
 
 	# latitude, longitude, modelFile
 	start = (10, 10)
-	end = (21, 15)
+	end = (7, 40)
 	modelFile = 'logisticModel.pkl'
 
-	ucs = searchUtil.UniformCostSearch(verbose=3)
-	ucs.solve(ShortestPath(start, end, locationGrid))
+	ucs = searchUtil.UniformCostSearch(verbose=0)
 
+	# shortest path
+	ucs.solve(ShortestPath(start, end, locationGrid))
+	actions = ucs.actions
+	displayResults(actions, start)
+
+	# safest path
+	ucs.solve(SafestPath(start, end, locationGrid))
 	actions = ucs.actions
 	print ' '.join(actions)
+
